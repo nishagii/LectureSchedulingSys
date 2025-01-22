cc0545df2d36fc3de840a62134d8ae531fa0db2e
nishagii <jeewanthadeherath04@gmail.com>
2025-01-22 21:42:49 +0530

    Add migrations for students and lectures tables

---
+++ database/migrations/2025_01_22_093826_create_lectures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->id();
+            $table->string('lecture_name');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('lecture_week');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+}
