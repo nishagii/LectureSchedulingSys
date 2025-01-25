86dd67aa05f200984848760594e7064dd9491322
nadun_hasalanka <nadun202327@gmail.com>
2025-01-25 19:11:38 +0530

    added +94 verification for phone number

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,14 +21,21 @@ class StudentController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:students,email',
             'course' => 'required|string|max:255',
-            'phone' => 'nullable|string|max:15',
+            'phone' => 'nullable|string|max:12',
         ]);
+
+        $phone = $request->phone;
+        if ($phone){
+            $phone = str_replace('+94', '', $phone);
+            $phone = ltrim($phone, '0');
+            $phone = '+94' . $phone;
+        }
 
         $student = new Student;
         $student->name = $request->name;
         $student->email = $request->email;
         $student->course = $request->course;
-        $student->phone = $request->phone;
+        $student->phone = $phone;
         $student->save();
 
         flash()->success('Success', 'Student Record has been created successfully !');
@@ -42,13 +49,20 @@ class StudentController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:students,email,' . $student->id,
             'course' => 'required|string|max:255',
-            'phone' => 'nullable|string|max:15',
+            'phone' => 'nullable|string|max:12|regex:/^\+94\d+$/',
         ]);
+
+        $phone = $request->phone;
+        if ($phone){
+            $phone = str_replace('+94', '', $phone);
+            $phone = ltrim($phone, '0');
+            $phone = '+94' . $phone;
+        }
 
         $student->name = $request->name;
         $student->email = $request->email;
         $student->course = $request->course;
-        $student->phone = $request->phone;
+        $student->phone = $phone;
         $student->save();
 
         flash()->success('Success', 'Student Record has been Updated successfully !');
